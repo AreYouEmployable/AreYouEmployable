b3cd3177153cf9f9c388fd0f4777975fa33615cb
Chris McHardy <chris.mchardy@bbd.co.za>
2025-05-05 21:49:05 +0200

    App clean up

--- api/app.js
+++ api/app.js
@@ -1,40 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
-import assessmentRoutes from './routes/assessmentRoutes.js';
-
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-
 import routes from './routes/index.js';
 import swaggerOptions from './config/swagger.js';
-
-import authRoutes from './routes/authRoutes.js';
-import { verifyJWT } from './middlewares/authMiddleware.js';
 
 const app = express();
 dotenv.config();
 
-
 // Generate Swagger documentation
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
-// API Documentation
+// Documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/health', async (req, res) => {
     res.status(200).json({ message: 'API running and healthy' });
 });
 
-
-// API routes
+// Routes
 app.use('/api', routes);
-
-app.use('/api/auth', authRoutes);
-
-app.get('/protected', verifyJWT, (req, res) => {
-    res.json({ message: 'You are authenticated!', user: req.user });
-});
 
 app.listen(process.env.PORT, async () => { 
     if (process.env.ENVIRONMENT === 'production') {
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import usersRoutes from './userRoutes.js';
+import authRoutes from './authRoutes.js';
+import assessmentRoutes from './assessmentRoutes.js';
 
 const router = Router();
 
-// Mount API routes
+router.use('/auth', authRoutes);
 router.use('/users', usersRoutes);
+router.use('/assessment', assessmentRoutes);
 
 export default router;
