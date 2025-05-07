7a0f05132616b86cb3e336a95b5be302a2b4ae74
brilliant-bbd <162683143+brilliant-bbd@users.noreply.github.com>
2025-05-07 13:23:20 +0200

    Delete client/scripts/config.js

--- client/scripts/config.js
+++
@@ -1,5 +0,0 @@
-// Frontend configuration
-export const config = {
-    GOOGLE_CLIENT_ID: '509502634606-59ifco7766fs7th7l1nuso9hd9rnh9da.apps.googleusercontent.com', // Replace this with your actual Google Client ID
-    API_URL: 'http://localhost:3000'
-}; 
