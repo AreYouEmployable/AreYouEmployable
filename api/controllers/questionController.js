c3d130331d9c586ab12fa0fc22d2123aea13b173
Chris McHardy <chris.mchardy@bbd.co.za>
2025-05-09 11:10:32 +0200

    Remove log

--- api/controllers/questionController.js
+++ api/controllers/questionController.js
@@ -4,7 +4,6 @@ const getQuestion = async (req, res) => {
     try {
         const questionId = 2;
         const question = await questionService.getQuestion(questionId);
-        console.log(question)
         res.status(200).json(question);
     } catch (err) {
         res.status(400).json({ error: err.message });
