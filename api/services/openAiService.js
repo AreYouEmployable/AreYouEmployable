d89ce02f6ae82d2da1a2173e52283ee4fb02d91b
Alfred Malope <alfred.malope@vcontractor.co.za>
2025-05-15 13:14:23 +0200

    Update API

--- api/services/openAiService.js
+++ api/services/openAiService.js
@@ -1,5 +1,8 @@
 import OpenAI from "openai";
 import { getAssessmentUserAnswers } from "../repositories/answerRepository.js";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
